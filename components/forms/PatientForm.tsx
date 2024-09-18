4f6374eb6af0f0d935711fed39be6a77f0a30d88
Aman Verma <amanv9952@gmail.com>
2024-09-18 22:38:00 +0530

    commit
    page and patientform

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+"use client";
 
-const PatientForm = () => {
-  return (
-    <div>
-      Patirn
-    </div>
-  )
-}
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-export default PatientForm
+export const PatientForm = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const from = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+  return <div>PatientForm</div>;
+};
